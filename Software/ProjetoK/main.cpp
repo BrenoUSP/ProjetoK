18200373188b2945409ed2429a37a069bdf296a4
BrenoUSP <brenoslivio@usp.br>
2018-05-13 12:25:54 -0300

    SplashScreen estática adicionada.

--- Software/ProjetoK/main.cpp
+++ Software/ProjetoK/main.cpp
@@ -1,13 +1,28 @@
 #include "mainwindow.h"
 #include "texto.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <qthread.h>
+
+class I : public QThread
+{
+public:
+    static void sleep(unsigned long secs) { QThread::sleep(secs); }
+};
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPixmap pixmap("Resources/kiwiLogoGif.gif");
+    QSplashScreen splash(pixmap);
+
     a.setStyle("fusion");
     Texto t;
-    t.show();
 
+    splash.show();
+    I::sleep(5); // splash is shown for 5 seconds
+
+    t.show();
+    splash.finish(&t);
     return a.exec();
 }
